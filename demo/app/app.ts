fd2c5e38e40a464b0db5de892dae81b6eafbc291
EddyVerbruggen <eddyverbruggen@gmail.com>
2018-11-06 21:28:55 +0100

    wiring

--- demo/app/app.ts
+++ demo/app/app.ts
@@ -1,6 +1,6 @@
 ﻿import * as application from "tns-core-modules/application";
 
-// for delegate wiring
-// require('nativescript-snapkit/LoginKit');
+// for delegate wiring (without this Snap won't redirect back to the app after logging in)
+require('nativescript-snapkit');
 
 application.start({moduleName: "main-page"});
